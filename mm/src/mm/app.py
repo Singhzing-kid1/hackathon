d56589a78b8b6c3f82033373d363258f261d0fdc
Singhzing-kid1 <58711645+Singhzing-kid1@users.noreply.github.com>
2021-11-07 17:23:27 -0700

    added a .gitignore and a requirements.txt

--- mm/src/mm/app.py
+++ mm/src/mm/app.py
@@ -4,23 +4,38 @@ Hackathon project
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-import tinydb
+from tinydb import TinyDB, Query
+
+db = TinyDB('database.json')
+user = Query()
+users = db.table('User')
 
 class mm(toga.App):
 
+
     def startup(self):
-        """
-        Construct and show the Toga application.
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        if users.all() != '':
+            print('signed in')
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-        self.main_window.show()
+            main_box = toga.Box()
+
+            self.main_window = toga.MainWindow(title=self.formal_name)
+            self.main_window.content = main_box
+            self.main_window.show()
+
+        elif users.all() == ' ':
+            print('new user')
+            main_box = toga.Box()
+
+            self.main_window = toga.MainWindow(title=self.formal_name)
+            self.main_window.content = main_box
+            self.main_window.show()
+
+
+
+
+
 
 
 def main():
