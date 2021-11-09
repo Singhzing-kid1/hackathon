039cb68a83387cf617d735937ea338f40a61621a
Singhzing-kid1 <58711645+Singhzing-kid1@users.noreply.github.com>
2021-11-08 17:49:03 -0700

    added section w/ sample code

--- mm/src/mm/app.py
+++ mm/src/mm/app.py
@@ -18,10 +18,87 @@ class mm(toga.App):
         """
         main_box = toga.OptionContainer()
 
-        section1 = toga.Table(['checkin','journaling'])
-        section2 = toga.Tree(['Navigate'])
-        section3 = toga.Tree(['Navigate'])
-        section4 = toga.Tree(['Navigate'])
+        section1 = toga.Box(style=Pack(direction=COLUMN))
+        section2 = toga.Box(style=Pack(direction=COLUMN))
+        section3 = toga.Box(style=Pack(direction=COLUMN))
+        section4 = toga.Box(style=Pack(direction=COLUMN))
+
+        ######################################################################
+        # Section 1 -- Shanza Code Here Start
+        ######################################################################
+        sec1_label = toga.Label(
+            'sec1',
+            style=Pack(padding=(0, 5))
+        )
+
+        sec1_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        sec1_box.add(sec1_label)
+
+        section1.add(sec1_box)
+        ######################################################################
+        # Section 1 -- Shanza Code Here End
+        ######################################################################
+
+        ######################################################################
+        # Section 2 -- Rohan Code Here Start
+        ######################################################################
+        sec2_label = toga.Label(
+            'sec2',
+            style=Pack(padding=(0, 5))
+        )
+
+        sec2_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        sec2_box.add(sec2_label)
+
+        section2.add(sec2_box)
+
+
+
+        ######################################################################
+        # Section 2 -- Rohan Code Here End
+        ######################################################################
+
+        ######################################################################
+        # Section 3 -- Veer Code Here Start
+        ######################################################################
+        sec3_label = toga.Label(
+            'sec3',
+            style=Pack(padding=(0, 5))
+        )
+
+        sec3_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        sec3_box.add(sec3_label)
+
+        section3.add(sec3_box)
+
+
+
+        ######################################################################
+        # Section 3 -- Veer Code Here End
+        ######################################################################
+
+        ######################################################################
+        # Section 4 -- Veer Code Here Start
+        ######################################################################
+        sec4_label = toga.Label(
+            'sec4',
+            style=Pack(padding=(0, 5))
+        )
+
+        sec4_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        sec4_box.add(sec4_label)
+
+        section4.add(sec4_box)
+
+
+
+        ######################################################################
+        # Section 4 -- Veer Code Here End
+        ######################################################################
+
+
+
+
 
 
         main_box.add('Checkin/journaling', section1)
