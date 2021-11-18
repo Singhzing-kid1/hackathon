63a0e35813ab3683b04db6a36463582cdaad759d
Singhzing-kid1 <58711645+Singhzing-kid1@users.noreply.github.com>
2021-11-17 21:09:47 -0700

    . DOT LOL

--- mm/src/mm/app.py
+++ mm/src/mm/app.py
@@ -60,7 +60,7 @@ class mm(toga.App):
 
     def startup(self):
         """
-        Construct and show the Toga application.
+        Construct and show the Toga application
 
         Usually, you would add your application to a main content box.
         We then create a main window (with a name matching the app), and
